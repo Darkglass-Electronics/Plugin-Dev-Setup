1a29eeea99a428647f58a55b623bc7c888ed6f47
Bram Giesen <bramgiesen@proton.me>
2026-05-29 17:03:37 +0200

    Update tiny-host

--- lv2-validation/tiny-host/tiny-host.cpp
+++ lv2-validation/tiny-host/tiny-host.cpp
@@ -1,47 +1,17 @@
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
 #include <lilv/lilv.h>
+#include <lv2/atom/atom.h>
 #include <lv2/buf-size/buf-size.h>
 #include <lv2/options/options.h>
 #include <lv2/urid/urid.h>
-#include <lv2/options/options.h>
 #include <lv2/worker/worker.h>
-#include <cstdio>
+#include <sstream>
 #include <string>
 #include <vector>
-
-#define LV2_ATOM_URI    "http://lv2plug.in/ns/ext/atom"  ///< http://lv2plug.in/ns/ext/atom
-#define LV2_ATOM_PREFIX LV2_ATOM_URI "#"                 ///< http://lv2plug.in/ns/ext/atom#
-
-#define LV2_ATOM__Atom          LV2_ATOM_PREFIX "Atom"           ///< http://lv2plug.in/ns/ext/atom#Atom
-#define LV2_ATOM__AtomPort      LV2_ATOM_PREFIX "AtomPort"       ///< http://lv2plug.in/ns/ext/atom#AtomPort
-#define LV2_ATOM__Blank         LV2_ATOM_PREFIX "Blank"          ///< http://lv2plug.in/ns/ext/atom#Blank
-#define LV2_ATOM__Bool          LV2_ATOM_PREFIX "Bool"           ///< http://lv2plug.in/ns/ext/atom#Bool
-#define LV2_ATOM__Chunk         LV2_ATOM_PREFIX "Chunk"          ///< http://lv2plug.in/ns/ext/atom#Chunk
-#define LV2_ATOM__Double        LV2_ATOM_PREFIX "Double"         ///< http://lv2plug.in/ns/ext/atom#Double
-#define LV2_ATOM__Event         LV2_ATOM_PREFIX "Event"          ///< http://lv2plug.in/ns/ext/atom#Event
-#define LV2_ATOM__Float         LV2_ATOM_PREFIX "Float"          ///< http://lv2plug.in/ns/ext/atom#Float
-#define LV2_ATOM__Int           LV2_ATOM_PREFIX "Int"            ///< http://lv2plug.in/ns/ext/atom#Int
-#define LV2_ATOM__Literal       LV2_ATOM_PREFIX "Literal"        ///< http://lv2plug.in/ns/ext/atom#Literal
-#define LV2_ATOM__Long          LV2_ATOM_PREFIX "Long"           ///< http://lv2plug.in/ns/ext/atom#Long
-#define LV2_ATOM__Number        LV2_ATOM_PREFIX "Number"         ///< http://lv2plug.in/ns/ext/atom#Number
-#define LV2_ATOM__Object        LV2_ATOM_PREFIX "Object"         ///< http://lv2plug.in/ns/ext/atom#Object
-#define LV2_ATOM__Path          LV2_ATOM_PREFIX "Path"           ///< http://lv2plug.in/ns/ext/atom#Path
-#define LV2_ATOM__Property      LV2_ATOM_PREFIX "Property"       ///< http://lv2plug.in/ns/ext/atom#Property
-#define LV2_ATOM__Resource      LV2_ATOM_PREFIX "Resource"       ///< http://lv2plug.in/ns/ext/atom#Resource
-#define LV2_ATOM__Sequence      LV2_ATOM_PREFIX "Sequence"       ///< http://lv2plug.in/ns/ext/atom#Sequence
-#define LV2_ATOM__Sound         LV2_ATOM_PREFIX "Sound"          ///< http://lv2plug.in/ns/ext/atom#Sound
-#define LV2_ATOM__String        LV2_ATOM_PREFIX "String"         ///< http://lv2plug.in/ns/ext/atom#String
-#define LV2_ATOM__Tuple         LV2_ATOM_PREFIX "Tuple"          ///< http://lv2plug.in/ns/ext/atom#Tuple
-#define LV2_ATOM__URI           LV2_ATOM_PREFIX "URI"            ///< http://lv2plug.in/ns/ext/atom#URI
-#define LV2_ATOM__URID          LV2_ATOM_PREFIX "URID"           ///< http://lv2plug.in/ns/ext/atom#URID
-#define LV2_ATOM__Vector        LV2_ATOM_PREFIX "Vector"         ///< http://lv2plug.in/ns/ext/atom#Vector
-#define LV2_ATOM__atomTransfer  LV2_ATOM_PREFIX "atomTransfer"   ///< http://lv2plug.in/ns/ext/atom#atomTransfer
-#define LV2_ATOM__beatTime      LV2_ATOM_PREFIX "beatTime"       ///< http://lv2plug.in/ns/ext/atom#beatTime
-#define LV2_ATOM__bufferType    LV2_ATOM_PREFIX "bufferType"     ///< http://lv2plug.in/ns/ext/atom#bufferType
-#define LV2_ATOM__childType     LV2_ATOM_PREFIX "childType"      ///< http://lv2plug.in/ns/ext/atom#childType
-#define LV2_ATOM__eventTransfer LV2_ATOM_PREFIX "eventTransfer"  ///< http://lv2plug.in/ns/ext/atom#eventTransfer
-#define LV2_ATOM__frameTime     LV2_ATOM_PREFIX "frameTime"      ///< http://lv2plug.in/ns/ext/atom#frameTime
-#define LV2_ATOM__supports      LV2_ATOM_PREFIX "supports"       ///< http://lv2plug.in/ns/ext/atom#supports
-#define LV2_ATOM__timeUnit      LV2_ATOM_PREFIX "timeUnit"       ///< http://lv2plug.in/ns/ext/atom#timeUnit
+#include <fstream>
 
 enum {
     k_urid_null,
@@ -51,6 +21,18 @@ enum {
     k_urid_atom_float,
     k_urid_atom_double,
 };
+
+// Helper to hash uri, to create a unique but safe filename
+static std::string hash_uri(const std::string& s) {
+    uint64_t h = 0xcbf29ce484222325ULL;
+    for (unsigned char c : s) {
+        h ^= c;
+        h *= 0x100000001b3ULL;
+    }
+    std::ostringstream out;
+    out << std::hex << std::setw(16) << std::setfill('0') << h;
+    return out.str();
+}
 
 static LV2_URID mapfn(LV2_URID_Map_Handle, const char* const uri)
 {
@@ -112,8 +94,15 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    const char* bundle_path   = argv[1];
+    std::string msg;
+
+    const char* bundle_path    = argv[1];
     const char* plugin_uri_str = argv[2];
+
+    // If we actually have warnings / pedantic info later, we can write those to a different file.
+    // So that the python script has an easy way to differentiate.
+    const std::string filename_err = "/tmp/" + hash_uri(plugin_uri_str)
+        + "_tinyhost_testresult_err.txt";
 
     LilvWorld* world = lilv_world_new();
     LilvNode* bundle_uri = lilv_new_file_uri(world, nullptr, bundle_path);
@@ -123,56 +112,147 @@ int main(int argc, char* argv[])
     LilvNode* plugin_uri = lilv_new_uri(world, plugin_uri_str);
     const LilvPlugins* plugins = lilv_world_get_all_plugins(world);
     const LilvPlugin* plugin = lilv_plugins_get_by_uri(plugins, plugin_uri);
+    LilvInstance* instance = nullptr;
 
-    if (!plugin) {
-        fprintf(stderr, "Plugin not found\n");
+    auto cleanup = [&] {
+        if (instance) lilv_instance_free(instance);
         lilv_node_free(plugin_uri);
         lilv_node_free(bundle_uri);
         lilv_world_free(world);
+    };
+
+    auto write_err_msg = [&]() {
+        std::ofstream out(filename_err);
+        if (out) out << msg;
+    };
+
+    if (!plugin) {
+        msg += "Plugin not found\n";
+        cleanup();
         return 1;
     }
 
     LV2_URID_Map map = { nullptr, mapfn };
-    LV2_Feature map_feature = { LV2_URID__map, &map };
+
+    LV2_Feature map_feature = {
+        LV2_URID__map,
+        &map
+    };
+
+    LV2_Feature bounded_block_length_feature = {
+        LV2_BUF_SIZE__boundedBlockLength, nullptr
+    };
+
+    LV2_Feature license_feature = {
+        "http://moddevices.com/ns/ext/license#feature", nullptr
+    };
 
     LV2_Worker_Schedule worker_schedule = { nullptr, worker_schedule_func };
     LV2_Feature worker_feature = { LV2_WORKER__schedule, &worker_schedule };
 
-    const LV2_Feature* features[] = { &map_feature, &worker_feature, &options_feature, nullptr };
+    const LV2_Feature* features[] = {
+        &map_feature,
+        &worker_feature,
+        &options_feature,
+        &bounded_block_length_feature,
+        &license_feature,
+        nullptr
+    };
 
-    LilvInstance* instance = lilv_plugin_instantiate(plugin, 48000.0, features);
-    if (!instance) {
-        fprintf(stderr, "Failed to instantiate plugin\n");
-        lilv_node_free(plugin_uri);
-        lilv_node_free(bundle_uri);
-        lilv_world_free(world);
+    bool features_missing = false;
+    // Check required features before instantiate, so a missing feature gives
+    // an informative error rather than a silent NULL.
+    LilvNodes* required = lilv_plugin_get_required_features(plugin);
+    LILV_FOREACH(nodes, i, required) {
+        const char* uri = lilv_node_as_uri(lilv_nodes_get(required, i));
+        if (!uri) continue;
+
+        bool supplied = false;
+        for (const LV2_Feature* f : features) {
+            if (f && std::strcmp(f->URI, uri) == 0) { supplied = true; break; }
+        }
+        if (!supplied) {
+            msg += "Missing required feature: ";
+            msg += uri;
+            msg += "\n";
+            features_missing = true;
+        }
+    }
+    lilv_nodes_free(required);
+
+    if (features_missing != 0) {
+        cleanup();
+        write_err_msg();
         return 1;
     }
 
+    instance = lilv_plugin_instantiate(plugin, 48000.0, features);
+    if (!instance) {
+        msg += "Failed to instantiate plugin\n";
+        cleanup();
+        write_err_msg();
+        return 1;
+    }
+
+    // Check if the extension declaration matches between binary and TTL
+    const std::string target = "http://moddevices.com/ns/ext/license#interface";
+    bool inTtl = false;
     LilvNodes* extensions = lilv_plugin_get_extension_data(plugin);
     LILV_FOREACH(nodes, i, extensions) {
         const LilvNode* ext = lilv_nodes_get(extensions, i);
-        printf("Declared extension: %s\n", lilv_node_as_uri(ext));
-        const void* data = lilv_instance_get_extension_data(instance, lilv_node_as_uri(ext));
-        printf("  - Binary provides it: %s\n", data ? "yes" : "NO (mismatch!)");
+        const char* uri = lilv_node_as_uri(ext);
+        if (uri && target == uri) { inTtl = true; break; }
+    }
+    lilv_nodes_free(extensions);
+
+    const bool inBinary = lilv_instance_get_extension_data(instance, target.c_str()) != nullptr;
+
+    if (inTtl != inBinary) {
+        msg += "Declared extension: " + target + "\n  - ";
+        msg += (inTtl && !inBinary)
+             ? "In ttl but binary does NOT provide it (mismatch!)\n"
+             : "Binary provides it but ttl does NOT declare it (mismatch!)\n";
     }
 
-    float dummy[256] = {0};
+    // Connect ports, so we can see with a tool like valgrind if there is a memory issue here.
+    // No per-port validation here, since TTL validity is handled upstream by lv2-anagram-validate script.
     uint32_t num_ports = lilv_plugin_get_num_ports(plugin);
-    for (uint32_t i = 0; i < num_ports; i++) {
-        printf("checking port %d\n", i);
-        lilv_instance_connect_port(instance, i, dummy);
+    LilvNode* input_class = lilv_new_uri(world, LILV_URI_INPUT_PORT);
+    LilvNode* atom_class  = lilv_new_uri(world, LV2_ATOM__AtomPort);
+    const LV2_URID urid_atom_seq = mapfn(nullptr, LV2_ATOM__Sequence);
+
+    std::vector<std::vector<uint8_t>> port_buffers(num_ports);
+
+    for (uint32_t i = 0; i < num_ports; ++i) {
+        const LilvPort* port = lilv_plugin_get_port_by_index(plugin, i);
+        if (!port) continue;
+
+        const bool is_input = lilv_port_is_a(plugin, port, input_class);
+
+        if (lilv_port_is_a(plugin, port, atom_class)) {
+            constexpr size_t atom_buf_size = 1024;
+            port_buffers[i].resize(atom_buf_size, 0);
+            auto* seq = reinterpret_cast<LV2_Atom_Sequence*>(port_buffers[i].data());
+            seq->atom.type = urid_atom_seq;
+            seq->atom.size = is_input ? sizeof(LV2_Atom_Sequence_Body)
+                                       : atom_buf_size - sizeof(LV2_Atom);
+            if (is_input) { seq->body.unit = 0; seq->body.pad = 0; }
+        } else {
+            port_buffers[i].resize(block_length * sizeof(float), 0);
+        }
+
+        lilv_instance_connect_port(instance, i, port_buffers[i].data());
     }
+
+    lilv_node_free(input_class);
+    lilv_node_free(atom_class);
+
+    write_err_msg();
 
     lilv_instance_activate(instance);
     lilv_instance_run(instance, 256);
     lilv_instance_deactivate(instance);
-    lilv_instance_free(instance);
 
-    lilv_nodes_free(extensions);
-    lilv_node_free(plugin_uri);
-    lilv_node_free(bundle_uri);
-    lilv_world_free(world);
-
+    cleanup();
     return 0;
 }
